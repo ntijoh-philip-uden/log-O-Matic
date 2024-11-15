a4af2d3f5de6d89bd51ad10434a80e905ecbcc97
Philipuden <philip.uden@elev.ga.ntig.se>
2024-11-15 10:44:52 +0100

    Navbar done

--- Frontend/src/layouts/default.vue
+++ Frontend/src/layouts/default.vue
@@ -1,5 +1,32 @@
 <template>
   <v-app>
+    <v-header>
+      <v-app-bar :elevation="6">
+        <v-app-bar-title>Log-O-Matic</v-app-bar-title>
+
+        <template v-slot:append>
+          <v-btn v-if="Token && Teacher" @click="router.push('/Admin')">
+            <v-icon icon="mdi-cog" />
+            Admin
+          </v-btn>
+
+          <v-btn v-if="Token" @click="router.push('/Logs')">
+            <v-icon icon="mdi-format-list-bulleted" />
+            Logs
+          </v-btn>
+
+          <v-btn v-if="!Token" @click="router.push('/Login')">
+            <v-icon icon="mdi-lock-open" />
+            Sign in
+          </v-btn>
+
+          <v-btn v-if="Token" @click="router.push('/Logout')">
+            <v-icon icon="mdi-lock" />
+            Log out
+          </v-btn>
+        </template>
+      </v-app-bar>
+    </v-header>
     <v-main>
       <router-view />
     </v-main>
@@ -9,5 +36,7 @@
 </template>
 
 <script lang="ts" setup>
-  //
+  import router from '@/router'
+  const Token = ""
+  const Teacher = true
 </script>
